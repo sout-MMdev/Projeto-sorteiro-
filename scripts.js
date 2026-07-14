7d494afbd7c0d9f02593bd72a03991fffd618ea1
sout-MMdev <seu-email@exemplo.com>
2026-07-14 11:36:57 -0300

    atualizando

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 function generatorNumber() {
-      const min = Math.ceil(document.querySelector(".input-min").value)
-      const max = Math.floor(document.querySelector(".input-max").value)
-      const result =  Math.floor(Math.random() * (max - min + 1)) + min;
-  const showOutput = document.querySelector(".information-output").value = result;
+  const min = Math.ceil(document.querySelector(".input-min").value)
+  const max = Math.floor(document.querySelector(".input-max").value)
+        if ( min >= max) {
+              const showAlert = document.querySelector(".information-alert").value = " O valor minimo deve ser MENOR do que o valor máximo"
+            } 
+            else {
+                  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+                  const showOutput = document.querySelector(".information-output").value = result;
+                  }
 
-
-    
 }
